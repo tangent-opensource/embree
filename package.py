fba4a7e8845589121e48c434093b2ea5c5fe053c
Benjamin Skinner <benjamin.skinner96@gmail.com>
2020-10-07 18:20:05 -0400

    Initial rez package for embree

---
+++ package.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+name = 'embree'
+
+version = '3.8.0-ta.1.0.0'
+
+authors = [
+    'benjamin.skinner',
+]
+
+requires = [
+    'tbb-2019'
+]
+
+@early()
+def private_build_requires():
+    import sys
+    if 'win' in str(sys.platform):
+        return ['visual_studio']
+    else:
+        return ['gcc-7']
+
+variants = [
+    ['platform-windows', 'arch-x64', 'os-windows-10'],
+    #['platform-linux', 'arch-x64'],
+]
+
+build_system = "cmake"
+
+def commands():
+
+    # Split and store version and package version
+    split_versions = str(version).split('-')
+    env.EMBREE_VERSION.set(split_versions[0])
+    env.EMBREE_PACKAGE_VERSION.set(split_versions[1])
+
+    env.EMBREE_ROOT.set("{root}")
+    env.EMBREE_INCLUDE_DIR.set("{root}/include")
+    env.EMBREE_LIBRARY_DIR.set("{root}/lib")
+    env.EMBREE_BINARY_DIR.set("{root}/bin")
+
+    env.PATH.append( str(env.EMBREE_BINARY_DIR) )
